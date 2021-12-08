74b1d9d9dc818286b2878e423ef43ee754de1cdc
hsqStephenZhang <2250015961@qq.com>
2021-12-08 11:58:07 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
 pub mod breakpoint;
 pub mod debugger;
 
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
-    println!("Hello, world!");
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new();
+    if rl.load_history(".gdbrs.history").is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline("minidbg>> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+    rl.save_history(".gdbrs.history").unwrap();
 }
