f40959ba0006dda6929035be14dcb8900ce40215
hsqStephenZhang <2250015961@qq.com>
2021-12-10 10:55:56 +0000

    remove goblin, add object, useing gimli

--- data/test.c
+++ data/test.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void f1(){
+
+}
+
+int f2(){
+    return 1;
+}
+
 int main(){
     int a=1;
     int b=2;
@@ -8,5 +16,8 @@ int main(){
     c=4;
     printf("%d\n",c);
     c=5;
+    printf("%d\n",c);
+    f1();
+    c=f2();
     printf("%d\n",c);
 }
